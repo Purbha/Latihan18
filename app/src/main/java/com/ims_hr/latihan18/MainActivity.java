605150c764f4557a9949007032af9ca85b1f1b24
Purbha <purbha_6string@yahoo.co.id>
2020-12-13 17:00:59 +0700

    UPDATE 20201213 17:00

--- app/src/main/java/com/ims_hr/latihan18/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan18/MainActivity.java
@@ -14,8 +14,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button B_OK1;
-    Button B_OK2;
+    Button B_OK1, B_OK2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void Inisial(){
-        B_OK1 = (Button) findViewById(R.id.button_Main_OK1);
-        B_OK2 = (Button) findViewById(R.id.button_Main_OK2);
+        B_OK1 = findViewById(R.id.button_Main_OK1);
+        B_OK2 = findViewById(R.id.button_Main_OK2);
     }
 
     private void Listen_B_OK2() {
@@ -45,26 +44,21 @@ public class MainActivity extends AppCompatActivity {
         B_OK1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String namaActivity = "com.ims_hr.app_satu.SatuActivity";
-                String namaPackage = "com.ims_hr.app_satu";
-                //Intent In = new Intent(namaDicari);
-                //startActivity(In);
+                String namaActivity = "com.ims_hr.latihan17.SatuActivity";
+                String namaPackage = "com.ims_hr.latihan17";
                 if(cekPackage(namaPackage) == true) {
-                    Intent In = new Intent(namaActivity); startActivity(In);
+                    Intent In = new Intent(namaActivity);
+                    startActivity(In);
                 } else {
-                    //Instantiate an AlertDialog.Builder with its constructor
                     AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    //Chain together various setter methods to set the dialog characteristics
                     builder.setMessage("Aplikasi tidak ditemukan.");
                     builder.setTitle("Informasi");
-                    //Add the buttons
                     builder.setPositiveButton("Okesip", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             dialog.dismiss();
                         }
                     });
-                    //Get the AlertDialog from create()
                     AlertDialog dialog = builder.create(); dialog.show();
                 }
             }
@@ -75,14 +69,6 @@ public class MainActivity extends AppCompatActivity {
         boolean Hasil = false;
         List<ApplicationInfo> ListAplikasi;
         PackageManager packageManager = getPackageManager();
-        /*
-        getInstalledApplications --> added in API level 1
-        public abstract List<ApplicationInfo> getInstalledApplications (int flags)
-        Return a List of all application packages that are installed for the current user.
-        If flag GET_UNINSTALLED_PACKAGES has been set, a list of all applications including those
-            deleted with DONT_DELETE_DATA (partially installed apps with data directory)
-            will be returned.
-        */
         ListAplikasi = packageManager.getInstalledApplications(0);
         for(ApplicationInfo info : ListAplikasi) {
             if(info.packageName.equals(namaPackage)) {
