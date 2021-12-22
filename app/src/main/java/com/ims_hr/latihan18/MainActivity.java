c79bbd887e0f787c29d0e8d6a5286b229556d8ce
Irsyad Purbha <irsyadpurbha@gmail.com>
2021-12-22 22:55:50 +0700

    UPDATE 20211222 21:07

--- app/src/main/java/com/ims_hr/latihan18/MainActivity.java
+++ app/src/main/java/com/ims_hr/latihan18/MainActivity.java
@@ -5,10 +5,11 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.List;
 
